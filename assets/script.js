5c5b60eae8d65156859b691a140def1241645aaa
macrowil <macrow.co@gmail.com>
2023-12-08 12:31:28 -0700

    changed buttons around in html and added classes for appending info

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
 //API for yelp
 
-const options = {
-     method: 'GET',
-     headers: {
-       accept: 'application/json',
-       Authorization: 'Bearer WrsNmnOdmTrlPocLosX8oMYhWXd3iZLTlz2NUkekcC94AdSYGQctfpbSGlJPOG2mvyWmXFIJVJBgdQxXzNMIztYgbjhyGbJaGPqPHFVwYRTwOMbAtGXWFHuGPepwZXYx'
-     }
-   };
-   
-   fetch('https://api.yelp.com/v3/businesses/search?location=Denver&sort_by=best_match&limit=20', options)
-     .then(response => response.json())
-     .then(response => console.log(response))
-     .catch(err => console.error(err));
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer WrsNmnOdmTrlPocLosX8oMYhWXd3iZLTlz2NUkekcC94AdSYGQctfpbSGlJPOG2mvyWmXFIJVJBgdQxXzNMIztYgbjhyGbJaGPqPHFVwYRTwOMbAtGXWFHuGPepwZXYx",
+//   },
+// };
+
+// fetch(
+//   "https://api.yelp.com/v3/businesses/search?location=Denver&sort_by=best_match&limit=20",
+//   options
+// )
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
 
 //API for Location
+
+function displayData() {
+  $(".weatherForecast").show();
+  $(".container1").show();
+  var displayImage = "http://www.w3.org/2000/svg";
+
+  $(".today").empty();
+
+  $(".col1").children("div").children("svg").children("text").text("beans");
+}
+displayData();
